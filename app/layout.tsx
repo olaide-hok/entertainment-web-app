1da33c9edcf68119f545dca9d0619a90bd77a89e
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-08-03 09:02:57 +0100

    initial setup

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type {Metadata} from 'next';
+import {Outfit} from 'next/font/google';
+import './globals.css';
+
+const outfitSans = Outfit({
+    variable: '--ff-outfit',
+    subsets: ['latin'],
+    display: 'swap',
+});
+
+export const metadata: Metadata = {
+    title: 'An Entertainment App',
+    description: 'An entertainment web app',
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={`${outfitSans.variable} antialiased`}>
+                {children}
+            </body>
+        </html>
+    );
+}
