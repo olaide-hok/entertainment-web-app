dc91e42eff89f1579010a3b804996d93f1169712
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-08-05 11:00:11 +0100

    setup Button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,30 @@
-const Button = () => {
-    return <div>Button</div>;
+'use client';
+
+interface ButtonProps {
+    variant: 'primary';
+    name: string;
+    styles?: string;
+}
+
+const variants = {
+    primary:
+        'bg-(--clr-white) text-(--clr-blue-900) hover:bg-(--clr-red-500) hover:text-white text-(length:--fs-15) py-[0.875rem] rounded-[0.375rem]',
+};
+
+const Button = ({
+    variant = 'primary',
+    name,
+    styles,
+    ...props
+}: React.ComponentProps<'button'> & ButtonProps) => {
+    return (
+        <button
+            type="button"
+            className={`${variants[variant]}  font-light cursor-pointer ${styles}`}
+            {...props}>
+            {name}
+        </button>
+    );
 };
 
 export default Button;
