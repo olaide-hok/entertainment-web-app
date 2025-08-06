5b980041b6a5677c9743db14855caf5828ae18e7
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-08-06 18:47:19 +0100

    update TVSeries component

--- app/(root)/tv-series/page.tsx
+++ app/(root)/tv-series/page.tsx
@@ -1,5 +1,37 @@
+'use client';
+
+import FilmTile from '@/components/FilmTile';
+import SearchBar from '@/components/SearchBar';
+import {useFilmStore} from '@/store/filmStore';
+
 const TVSeries = () => {
-    return <div>TVSeries</div>;
+    const {tvSeries} = useFilmStore();
+
+    return (
+        <div className="flex flex-col gap-y-(--space-500)">
+            <SearchBar />
+            <div className="flex flex-col gap-y-(--space-400)">
+                <h2 className="text-(length:--fs-32) text-(--clr-white) font-light leading-(--lh-125) trailing-(--ls-5)">
+                    TV Series
+                </h2>
+
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-(--space-200) md:gap-y-(--space-300) gap-x-(--space-200) lg:gap-x-(--space-500)">
+                    {tvSeries.map((series) => (
+                        <FilmTile
+                            key={series.title}
+                            title={series.title}
+                            year={series.year}
+                            category={series.category}
+                            rating={series.rating}
+                            thumbnail={series.thumbnail}
+                            isBookmarked={series.isBookmarked}
+                            isTrending={series.isTrending}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default TVSeries;
