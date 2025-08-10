9899876aea5e16199a408c0ba0c6fd2ecc454803
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-08-10 08:46:51 +0100

    update handleBookmark Fn to use toggleBookmark store Fn

--- components/TrendingFilmTile.tsx
+++ components/TrendingFilmTile.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import SVGIcons from './SVGIcons';
 import {useState} from 'react';
 
-import {Film as TrendingFilmTileProps} from '@/store/filmStore';
+import {Film as TrendingFilmTileProps, useFilmStore} from '@/store/filmStore';
 
 const TrendingFilmTile = ({
     title,
@@ -14,15 +14,18 @@ const TrendingFilmTile = ({
     isTrending,
 }: TrendingFilmTileProps) => {
     const [activeIcon, setActiveIcon] = useState<boolean>(isBookmarked);
-    const handleBookmark = () => {
+    const {toggleBookmark} = useFilmStore();
+
+    const handleBookmark = (title: string) => {
         setActiveIcon(!activeIcon);
+        toggleBookmark(title);
     };
 
     if (isTrending && thumbnail.trending) {
         return (
             <div className="flex flex-col gap-y-(--space-100)">
                 {/* Image */}
-                <div className="relative md:w-[29.375rem] md:h-[14.375rem]">
+                <div className="relative w-full h-[8.75rem] md:w-[29.375rem] md:h-[14.375rem]">
                     <Image
                         className="rounded-(--space-100) object-cover"
                         src={thumbnail?.trending?.large}
@@ -30,12 +33,12 @@ const TrendingFilmTile = ({
                         fill
                     />
                     <div className="absolute flex flex-col justify-between h-full w-full px-(--space-300) py-(--space-200)">
-                        <div className="bookmark-svg flex items-center justify-center bg-(--clr-blue-950)/50 cursor-pointer hover:bg-(--clr-white) rounded-full w-(--space-400) h-(--space-400)">
+                        <div className="bookmark-svg flex items-center self-end justify-center bg-(--clr-blue-950)/50 cursor-pointer hover:bg-(--clr-white) rounded-full w-(--space-400) h-(--space-400)">
                             <button
                                 type="button"
                                 className="cursor-pointer"
                                 aria-label="bookmark"
-                                onClick={handleBookmark}>
+                                onClick={() => handleBookmark(title)}>
                                 <SVGIcons
                                     name={
                                         activeIcon
