ed50dd8078b3990c0ddca84d4a8147734f2522b4
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-08-15 10:24:21 +0100

    wrap app with AuthProviders

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import {Outfit} from 'next/font/google';
 import './globals.css';
+import {AuthProviders} from '@/components/providers';
 
 const outfitSans = Outfit({
     variable: '--ff-outfit',
@@ -21,7 +22,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={`${outfitSans.variable} antialiased`}>
-                {children}
+                <AuthProviders>{children}</AuthProviders>
             </body>
         </html>
     );
