5534db4ad5d919a0bfd9fbff9039aa0b0532b163
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-08-15 10:12:04 +0100

    update utility classes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <main className="container flex flex-col h-full justify-center gap-y-(--space-300) md:gap-y-(--space-400) xl:gap-y-[4.3125rem] gap-x-[2.25rem] xl:flex-row lg:pl-(--space-400) lg:mt-(--space-400) mb-[4.3125rem]">
+        <main className="container justify-self-center flex flex-col h-full justify-center gap-y-(--space-300) md:gap-y-(--space-400) xl:gap-y-[4.3125rem] gap-x-[2.25rem] xl:flex-row lg:pl-(--space-400) lg:mt-(--space-400) mb-[4.3125rem]">
             <Navbar />
             {children}
         </main>
