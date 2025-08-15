154d1ea7ca684bac12f890308293e3f199be1287
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-08-15 10:14:52 +0100

    update BUtton component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,26 +1,30 @@
 'use client';
 
+import {cn} from '@/lib/utils';
+
 interface ButtonProps {
-    variant: 'primary';
+    variant: 'primary' | 'secondary';
     name: string;
-    styles?: string;
+    className?: string;
 }
 
 const variants = {
     primary:
         'bg-(--clr-white) text-(--clr-blue-900) hover:bg-(--clr-red-500) hover:text-white text-(length:--fs-15) py-[0.875rem] rounded-[0.375rem]',
+    secondary:
+        'bg-(--clr-red-500) text-(--clr-white) hover:bg-(--clr-white) hover:text-(--clr-blue-900) text-(length:--fs-15) py-[0.875rem] rounded-[0.375rem]',
 };
 
 const Button = ({
     variant = 'primary',
     name,
-    styles,
+    className,
     ...props
 }: React.ComponentProps<'button'> & ButtonProps) => {
     return (
         <button
             type="button"
-            className={`${variants[variant]}  font-light cursor-pointer ${styles}`}
+            className={cn(`${variants[variant]} ${className} font-light`)}
             {...props}>
             {name}
         </button>
